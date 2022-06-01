62c3e0881ac7049d154663ee72907273ac636b1a
CFANNIN6 <chris.fannin@kizan.com>
2022-06-01 17:44:58 -0400

    Added meta file; updated URLs in script

---
+++ fpie/FULLBEAUTY Product Image Extractor.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         FULLBEAUTY Product Image Extractor
+// @namespace    https://www.facebook.com/ThoughtWinds/
+// @website      https://github.com/Abbydon/tm_userscripts/
+// @version      1.01
+// @description  Provides options in the breadcrumb for viewing and saving the full-sized product image from all 12 FULLBEAUTY Brands sites.
+// @author       Chris Fannin
+// @copyright    GNU GPLv3
+// @updateURL    https://github.com/Abbydon/tm_userscripts/blob/main/fpie/FULLBEAUTY%20Product%20Image%20Extractor.meta.js
+// @downloadURL  https://github.com/Abbydon/tm_userscripts/blob/main/fpie/FULLBEAUTY%20Product%20Image%20Extractor.user.js
+// ==/UserScript==
